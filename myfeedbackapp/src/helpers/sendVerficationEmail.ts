675bfb73d987921c0e5db374a326bf7506fb5aaf
Vansh Mehta <mehtavansh2211@gmail.com>
2025-11-29 13:55:18 +0530

    db connect and email

---
+++ myfeedbackapp/src/helpers/sendVerficationEmail.ts
@@ -0,0 +1,25 @@
+import {resend} from '../lib/resend';
+import {VerificationEmail} from '../../emails/VerficationEmail';
+import {ApiResponse} from '../types/ApiResponse';
+import { ca } from 'zod/locales';
+
+export async function sendVerificationEmail(
+    email:string, 
+    username:string, 
+    verifyCode:string
+):Promise<ApiResponse> {
+    try{
+        return {
+            success: true,
+            message: "Verification email sent successfully" 
+        }
+    }
+    catch(emailError){
+        console.error("Error sending verification email:", emailError);
+
+        return{
+            success: false,
+            message: "Failed to send verification email"   
+        }
+    }
+}
